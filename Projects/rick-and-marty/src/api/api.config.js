9f68125f0040089c3e876b103fd119c443eab88c
Eray Demircioğlu <75967993+eraydmrcoglu@users.noreply.github.com>
2022-06-02 12:32:07 +0300

    Delete Projects/rick-and-marty directory

--- Projects/rick-and-marty/src/api/api.config.js
+++
@@ -1,7 +0,0 @@
-const BASE_URL = "https://rickandmortyapi.com/api";
-
-export const API_URL = {
-  characters: `${BASE_URL}/character`,
-  locations: `${BASE_URL}/location`,
-  episodes: `${BASE_URL}/episode`,
-};
