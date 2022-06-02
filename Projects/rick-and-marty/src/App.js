c8400ecda4d76744db257838073ce8e14df680d6
Eray Demircioğlu <75967993+eraydmrcoglu@users.noreply.github.com>
2022-06-02 12:23:34 +0300

    Add files via upload

---
+++ Projects/rick-and-marty/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import { Container } from "@chakra-ui/react";
+import { ErrorBoundary } from "./components/ErrorBoundary/ErrorBoundary";
+import RickMortyContainer from "./containers/RickMortyContainer";
+
+function App() {
+  return (
+    <ErrorBoundary>
+      <Container maxW="container.lg" my={"30px"} pt={"20px"}>
+        <RickMortyContainer />
+      </Container>
+    </ErrorBoundary>
+  );
+}
+
+export default App;
