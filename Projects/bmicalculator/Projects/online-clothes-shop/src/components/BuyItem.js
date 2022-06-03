0d5d31212462440a81af464f5842bb708175161f
Eray Demircioğlu <75967993+eraydmrcoglu@users.noreply.github.com>
2022-06-03 11:15:30 +0300

    Add files via upload

---
+++ Projects/bmicalculator/Projects/online-clothes-shop/src/components/BuyItem.js
@@ -0,0 +1,28 @@
+import React, { useContext } from "react";
+import { Context } from "../context/ProductsProvider";
+
+const BuyItem = ({ item }) => {
+  const { deleteBasket } = useContext(Context);
+
+  return (
+    <div className="buy-item">
+      <div
+        onClick={() => deleteBasket(item.id, item.price)}
+        className="buy-item__delete"
+      >
+        <i className="fas fa-times fa-2x"></i>
+      </div>
+      <img className="buy-item__img" src={item.image} alt={item.title} />
+      <div className="buy-item__details">
+        <h2 className="buy-item__details--title">{item.title}</h2>
+        <p className="buy-item__details-content">
+          M | Front print and paisley print
+        </p>
+        <span className="buy-item__details-count">Quantity: 1</span>
+      </div>
+      <div className="buy-item__price">{item.price} $</div>
+    </div>
+  );
+};
+
+export default BuyItem;
---
+++ Projects/bmicalculator/Projects/online-clothes-shop/src/components/Product.js
@@ -0,0 +1,54 @@
+import React, { useContext } from "react";
+import { Context } from "../context/ProductsProvider";
+import { Button } from "./Button";
+
+const Product = (props) => {
+  const product = props.product;
+
+  const { addBasket, addFavorite, basket, favorites } = useContext(Context);
+
+  const handleBasket = (product) => {
+    const basketHasProduct = basket.some(({ id }) => id === product.id);
+    if (!basketHasProduct) {
+      addBasket(product);
+    }
+  };
+
+  const handleFavorite = (product) => {
+    const favoriteHasProduct = favorites.some(({ id }) => id === product.id);
+    if (!favoriteHasProduct) {
+      addFavorite(product);
+    }
+  };
+
+  return (
+    <div className="product-item">
+      <img
+        src={product.image}
+        className="product-item__img"
+        alt={product.title}
+      />
+      <div className="product-item__price">
+        <span className="product-item__price--title">{product.title}</span>
+        <span className="product-item__price--count">{product.price} $</span>
+        <Button
+          onClick={() => handleBasket(product)}
+          className="product-item__price--button"
+          value={product.id}
+        >
+          Şimdi Satın Al
+        </Button>
+        <Button
+          green
+          onClick={() => handleFavorite(product)}
+          className="product-item__price--button"
+          value={product.id}
+        >
+          Favorilere Ekle
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default Product;
